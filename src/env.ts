310e155d2af94d6300b686a28790f66de11bd68b
Júlio Cézar <jczrpereira77@gmail.com>
2025-03-02 09:08:25 -0300

    feat(database): update environment configuration for PostgreSQL and add migrations for users, gyms, and check-ins

--- src/env.ts
+++ src/env.ts
@@ -7,12 +7,9 @@ const envSchema = z.object({
   // Database
   DATABASE_URL: z.string(),
 
-  // Redis
-  REDIS_URL: z.string().url(),
-
   // URLs
-  API_URL: z.string().url(),
-  WEB_URL: z.string().url(),
+  // API_URL: z.string().url(),
+  // WEB_URL: z.string().url(),
 })
 
 export const env = envSchema.parse(process.env)
